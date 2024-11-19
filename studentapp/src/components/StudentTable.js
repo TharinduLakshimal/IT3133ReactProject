2882a02a690204e33a6b1a4e45a07f5b8433caef
TharinduLakshimal <139586285+TharinduLakshimal@users.noreply.github.com>
2024-11-19 17:18:48 +0000

    view button added

--- studentapp/src/components/StudentTable.js
+++ studentapp/src/components/StudentTable.js
@@ -12,6 +12,7 @@ export default function StudentTable(props){
                             <td>Last Name</td>
                             <td>course</td>
                             <td>Country</td>
+                            <td>Profile</td>
                          
                         </tr>
                     </thead>
@@ -23,6 +24,7 @@ export default function StudentTable(props){
                             <td>{student.lastName}</td>
                             <td>{student.course}</td>
                             <td>{student.address.country}</td>
+                            <td><button>View</button></td>
                           
                         </tr>
                         ) 
